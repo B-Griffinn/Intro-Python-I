651405cfd97674dee5f71061c64b716ee7b882ca
Ben <bigriffin94@outlook.com>
2020-05-04 12:23:44 -0700

    00-05 complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,29 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+x.append(4)
+print('Append 4 to x', x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+x.extend(y)
+print('extend y to x', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+x.remove(8)
+print('8 has been removed from x', x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(len(x) - 1, 99)
+print('insert 99 into length of x - 1', x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(f"The length of x is {len(x)}")
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for i in range(len(x)):
+    print(f"{x[i]} * 1000 is", x[i] * 1000)
